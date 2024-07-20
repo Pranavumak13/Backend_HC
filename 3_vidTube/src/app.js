8d10a0aa7aa320dd7b60a6589acb29983b308708
Pranavumak13 <2020bcs077@sggs.ac.in>
2024-07-20 16:54:37 +0530

    Feat - Add the DB Connection to VidTube Project

---
+++ 3_vidTube/src/app.js
@@ -0,0 +1,24 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+// who should be able to talk to the db --> CORS {Cross Origin Resource Sharing}
+
+// handling the middleware we always use app.use()
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials:true
+    })
+)
+
+//another middleware for express
+app.use(express.json({limit:"16kb"})) // limit on json content
+app.use(express.urlencoded({ //encoding the url for eg %20 in the url
+    extended:true, 
+    limit:"16kb"
+}))
+app.use(express.static("public")) //all media files here
+
+export {app}
--- 3_vidTube/src/index.js
+++ 3_vidTube/src/index.js
@@ -1,1 +1,19 @@
-console.log('Hello From Pranav Umak');
+import {app} from './app.js';
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+
+// to config the .env file.
+dotenv.config({
+  path: './.env',
+});
+const PORT = process.env.PORT || 8080;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}...........`);
+    });
+  })
+  .catch(err => {
+    console.log('MongoDB Connection error', err);
+  });
